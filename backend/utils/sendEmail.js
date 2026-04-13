4e874d1ae45b719739aad078996b9d146c50f3d6
Shanmukh4449 <siddhartha.23bce9388@vitapstudent.ac.in>
2026-04-13 21:46:08 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,13 +3,15 @@ const nodemailer = require('nodemailer');
 const sendOTPEmail = async (to, otp) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      },
-      connectionTimeout: 10000, // ⏱ 10 sec timeout
-    });
+  host: 'smtp-relay.brevo.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  connectionTimeout: 10000,
+});
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
